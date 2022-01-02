2ab30c54e0131b9a756918636d134928301ea73e
mykyta <nikita.knyzevskiy@gmail.com>
2022-01-02 17:11:40 +0200

    Clear

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,7 +2,7 @@ object Config {
 
     const val minSdkVersion = "26"
     const val targetVersion = "30"
-    const val versionCode = 1
+    const val versionCode = 2
     const val versionName = "2.1.$versionCode"
 
 }
